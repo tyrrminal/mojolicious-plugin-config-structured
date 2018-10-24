a645fcc18b0aac71b3bde3438939e18cbbcee69e
Mark Tyrrell <MTyrrell@concertpharma.com>
2018-10-24 12:15:53 -0400

    handling config file locations better moving Concert::Config into companion module

--- lib/Mojolicious/Plugin/Concert/Config.pm
+++ lib/Mojolicious/Plugin/Concert/Config.pm
@@ -1,17 +1,16 @@
 package Mojolicious::Plugin::Concert::Config;
 use Mojo::Base 'Mojolicious::Plugin';
 
+use Concert::Constants;
+
 sub register {
   my ($self, $app) = @_;
 
   $app->helper(
     conf => sub {
-      my $home = Mojo::Home->new;
-      $home->detect;
-      my $f = File::Spec->catfile($home,$app->moniker.".conf");
       state $config = Concert::Config->new(
-        _conf => do $f,
-         _def => do "$f.def"
+         _conf => do File::Spec->catfile($app->home,join($PERIOD, $app->moniker, $app->mode, qw(conf))),
+         _def  => do File::Spec->catfile($app->home,join($PERIOD, $app->moniker, qw(conf def)))
       );
     }
   );
