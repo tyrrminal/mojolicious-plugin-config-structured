a7901eaced4283282d52abd5c4f68375aad9975d
Mark Tyrrell <MTyrrell@concertpharma.com>
2018-12-13 09:45:43 -0500

    improving logical flow and error reporting

--- lib/Mojolicious/Plugin/Concert/Config.pm
+++ lib/Mojolicious/Plugin/Concert/Config.pm
@@ -9,32 +9,43 @@ use Concert::Constants;
 sub register {
   my ($self, $app, $params) = @_;
 
-  my $conf;
+  my $conf = {};
   my @search = (
     $params->{config_file},
     File::Spec->catfile($app->home,join($PERIOD, $app->moniker, $app->mode, 'conf')),
     File::Spec->catfile($app->home,join($PERIOD, $app->moniker,             'conf'))
   );
-  foreach my $conf_file (grep {$_} @search) {
-    if(-e $conf_file) {
-      $app->log->info("Initializing Concert::Config from '$conf_file'");
-      $conf = do $conf_file; last;
-    }
+  my ($conf_file) = grep {$_ && -r -f $_} @search; #get the first extant, readable file
+  if(defined($conf_file)) {
+    $app->log->info("[Concert::Config] Initializing from '$conf_file'");
+    $conf = _parse_cfg_file($conf_file);
+  } else {
+    $app->log->error("[Concert::Config] Initializing with empty configuration");
   }
-  unless(defined($conf)) {
-    $conf = {};
-    $app->log->error("Initializing Concert::Config with empty configuration");
-  }
+
+  my $def = {};
   my $def_file = File::Spec->catfile($app->home,join($PERIOD, $app->moniker, qw(conf def)));
+  if(-r -f $def_file) {
+    $def = _parse_cfg_file($def_file);
+  } else {
+    $app->log->error("[Concert::Config] No configuration definition found (tried to read from `$def_file`)");
+  }
 
   $app->helper(
     conf => sub {
       state $config = Concert::Config->new(
          _conf => $conf,
-         _def  => do $def_file
+         _def  => $def
       );
     }
   );
+}
+
+
+# Someday we'll handle json/yml/xml?/ini? here, but for now just supports perl structure
+sub _parse_cfg_file {
+  my $f = shift;
+  return do $f;
 }
 
 1;
