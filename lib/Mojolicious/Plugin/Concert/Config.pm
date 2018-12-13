d8b03203708fd6851ac93597db5c4347618e2800
Mark Tyrrell <MTyrrell@concertpharma.com>
2018-12-13 08:47:31 -0500

    implementing conf file search list to pull from, in order: param-specified file, moniker.mode.conf, moniker.conf

--- lib/Mojolicious/Plugin/Concert/Config.pm
+++ lib/Mojolicious/Plugin/Concert/Config.pm
@@ -7,13 +7,31 @@ use Concert::Config;
 use Concert::Constants;
 
 sub register {
-  my ($self, $app) = @_;
+  my ($self, $app, $params) = @_;
+
+  my $conf;
+  my @search = (
+    $params->{config_file},
+    File::Spec->catfile($app->home,join($PERIOD, $app->moniker, $app->mode, 'conf')),
+    File::Spec->catfile($app->home,join($PERIOD, $app->moniker,             'conf'))
+  );
+  foreach my $conf_file (grep {$_} @search) {
+    if(defined($conf_file) && -e $conf_file) {
+      $app->log->info("Initializing Concert::Config from '$conf_file'");
+      $conf = do $conf_file; last;
+    }
+  }
+  unless(defined($conf)) {
+    $conf = {};
+    $app->log->error("Initializing Concert::Config with empty configuration");
+  }
+  my $def_file = File::Spec->catfile($app->home,join($PERIOD, $app->moniker, qw(conf def)));
 
   $app->helper(
     conf => sub {
       state $config = Concert::Config->new(
-         _conf => do File::Spec->catfile($app->home,join($PERIOD, $app->moniker, $app->mode, qw(conf))),
-         _def  => do File::Spec->catfile($app->home,join($PERIOD, $app->moniker, qw(conf def)))
+         _conf => $conf,
+         _def  => do $def_file
       );
     }
   );
