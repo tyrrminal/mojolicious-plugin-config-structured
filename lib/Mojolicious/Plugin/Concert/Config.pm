9793bfe48bd4eb1547f15967cea3b9c5da38be3f
Mark Tyrrell <mtyrrell@bobal.concertpharma.com>
2019-09-05 09:27:35 -0400

    utilizing new constants for clarity enforcing code symmetry between conf and def initialization

--- lib/Mojolicious/Plugin/Concert/Config.pm
+++ lib/Mojolicious/Plugin/Concert/Config.pm
@@ -5,15 +5,23 @@ use v5.22;
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 use Config::Structured;
 
-sub register {
-  my ($self, $app, $params) = @_;
+use Readonly;
+
+Readonly::Scalar our $PERIOD => q{.};
+
+Readonly::Scalar our $CONF_FILE_SUFFIX => q{conf};
+Readonly::Scalar our $DEF_FILE_SUFFIX  => q{def};
 
 sub register ($self, $app, $params) {
   my @search = (
     $params->{config_file},
-    $app->home->rel_file(join($PERIOD, $app->moniker, $app->mode, 'conf')),
-    $app->home->rel_file(join($PERIOD, $app->moniker,             'conf'))
+    $app->home->rel_file(join($PERIOD, $app->moniker, $app->mode, $CONF_FILE_SUFFIX)),
+    $app->home->rel_file(join($PERIOD, $app->moniker, $CONF_FILE_SUFFIX))
   );
+
+  my $conf = {};
+  my ($conf_file) = grep {$_ && -r -f $_} @search;    #get the first existent, readable file
+  if (defined($conf_file)) {
     $app->log->info("[Config::Structured] Initializing from '$conf_file'");
     $conf = _parse_cfg_file($conf_file);
   } else {
@@ -21,8 +29,8 @@ sub register ($self, $app, $params) {
   }
 
   my $def = {};
-  my $def_file = $app->home->rel_file(join($PERIOD, $app->moniker, qw(conf def)));
-  if(-r -f $def_file) {
+  my ($def_file) = grep {-r -f $_} $app->home->rel_file(join($PERIOD, $app->moniker, $CONF_FILE_SUFFIX, $DEF_FILE_SUFFIX));
+  if (defined($def_file)) {
     $def = _parse_cfg_file($def_file);
   } else {
     $app->log->error("[Config::Structured] No configuration definition found (tried to read from `$def_file`)");
