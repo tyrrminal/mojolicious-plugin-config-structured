2d22cdad432e68eaaf2976dd9b6df8f39cb36c46
Mark Tyrrell <mtyrrell@bobal.concertpharma.com>
2019-09-05 09:24:19 -0400

    refactoring Concert::Config references to Config::Structured removing Concert::Constants dependency adding Readonly dependency

--- lib/Mojolicious/Plugin/Concert/Config.pm
+++ lib/Mojolicious/Plugin/Concert/Config.pm
@@ -2,9 +2,7 @@ package Mojolicious::Plugin::Concert::Config;
 
 # ABSTRACT: Mojolicious Plugin for Concert::Config
 
-use Mojo::Base 'Mojolicious::Plugin';
-use Concert::Config;
-use Concert::Constants;
+use Config::Structured;
 
 sub register {
   my ($self, $app, $params) = @_;
@@ -15,12 +13,10 @@ sub register {
     $app->home->rel_file(join($PERIOD, $app->moniker, $app->mode, 'conf')),
     $app->home->rel_file(join($PERIOD, $app->moniker,             'conf'))
   );
-  my ($conf_file) = grep {$_ && -r -f $_} @search; #get the first extant, readable file
-  if(defined($conf_file)) {
-    $app->log->info("[Concert::Config] Initializing from '$conf_file'");
+    $app->log->info("[Config::Structured] Initializing from '$conf_file'");
     $conf = _parse_cfg_file($conf_file);
   } else {
-    $app->log->error("[Concert::Config] Initializing with empty configuration");
+    $app->log->error("[Config::Structured] Initializing with empty configuration");
   }
 
   my $def = {};
@@ -28,14 +24,14 @@ sub register {
   if(-r -f $def_file) {
     $def = _parse_cfg_file($def_file);
   } else {
-    $app->log->error("[Concert::Config] No configuration definition found (tried to read from `$def_file`)");
+    $app->log->error("[Config::Structured] No configuration definition found (tried to read from `$def_file`)");
   }
 
   $app->helper(
     conf => sub {
-      state $config = Concert::Config->new(
-         _conf => $conf,
-         _def  => $def
+      state $config = Config::Structured->new(
+        config_values => $conf,
+        definition    => $def
       );
     }
   );
